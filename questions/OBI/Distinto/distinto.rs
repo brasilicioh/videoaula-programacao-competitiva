51004d8ed3e16c6c96d9f8e85cbf2ff4074384e1
G-Aleixo <163425479+G-Aleixo@users.noreply.github.com>
2025-11-03 00:11:27 -0300

    problem distinto: add rust solution

---
+++ questions/OBI/Distinto/distinto.rs
@@ -0,0 +1,40 @@
+use std::collections::HashSet;
+
+fn main() {
+    let stdin = std::io::stdin();
+
+    let mut n = String::new();
+    stdin.read_line(&mut n).unwrap();
+
+    let n: usize = n.trim().parse().unwrap();
+
+    let mut i = vec![0; n];
+    let mut interval = HashSet::new();
+
+
+    for index in 0..i.len() {
+        let mut number = String::new();
+
+        stdin.read_line(&mut number).unwrap();
+        let number = number.trim().parse().unwrap();
+
+        i[index] = number;
+    };
+
+    let mut max_interval = 0;
+    let mut start = 0;
+
+    for end in 0..i.len() {
+        while interval.contains(&i[end]) {
+            interval.remove(&i[start]);
+            start += 1;
+        }
+
+        interval.insert(&i[end]);
+        max_interval = std::cmp::max(max_interval, end - start + 1);
+    }
+
+    println!("{max_interval}");
+}
+
+// Código não testado com o corretor automático
