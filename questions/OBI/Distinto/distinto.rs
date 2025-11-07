a1855eec6b7cd25188d04dce095f313776589015
brasilicioh <brasiliciohcr@gmail.com>
2025-11-06 22:33:31 -0300

    update rust solve

--- questions/OBI/Distinto/distinto.rs
+++ questions/OBI/Distinto/distinto.rs
@@ -1,19 +1,14 @@
 // Feito por Guilherme Aleixo
 
 use std::collections::HashSet;
-
 fn main() {
     let stdin = std::io::stdin();
-
     let mut n = String::new();
     stdin.read_line(&mut n).unwrap();
 
     let n: usize = n.trim().parse().unwrap();
-
     let mut i = vec![0; n];
     let mut interval = HashSet::new();
-
-
     for index in 0..i.len() {
         let mut number = String::new();
 
@@ -22,7 +17,6 @@ fn main() {
 
         i[index] = number;
     };
-
     let mut max_interval = 0;
     let mut start = 0;
 
@@ -31,11 +25,9 @@ fn main() {
             interval.remove(&i[start]);
             start += 1;
         }
-
         interval.insert(&i[end]);
         max_interval = std::cmp::max(max_interval, end - start + 1);
     }
-
     println!("{max_interval}");
 }
 
