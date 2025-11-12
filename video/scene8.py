c475c883b95619f1d1f5b9a1b33b9d187b45bf5f
brasilicioh <brasiliciohcr@gmail.com>
2025-11-12 02:55:29 -0300

    update scene 5

--- video/scene8.py
+++ video/scene8.py
@@ -1,95 +1,98 @@
-from moviepy import vfx # vfx para efeitos crossfadein/out; se necessario, separado por virgula, importe mais coisas
+from moviepy import vfx
 from clips import (
-    subtitle_clip, # para fazer legendas -> ja feito
-    audio_clip, # para fazer o audio -> ja feito
-    cat_clip, # retorna um ImageClip do gato
-    img_clip, # retorna um ImageClip de uma imagem
+    subtitle_clip,
+    audio_clip,
+    cat_clip,
+    img_clip,
     gif_clip,
-    video_clip, # junta todos ImageClip's e retorna um VideoClip
-    SCREEN_SIZE # tamanho constante de tela -> Full HD
+    video_clip,
+    SCREEN_SIZE
 )
 
-# aqui, utilize o cat_clip para fazer gatos
-# img_clip para fazer fotos e background
-# se necessario, importe outras coisas para fazer mais clips
-# veja a cena 1 e 2 para tomar como exemplo
-
-# recomendacao: na construcao do video, coloque as variaveis em ordem cronologica
-# se lembre de colocar todas variaveis no array clips seguindo a ordem de prioridade do array
-# ao terminar a codificacao, organize as variaveis em ordem igual a ordem do array
 wall_background = (
-    img_clip("img/background/wall.png", SCREEN_SIZE, 0, 19.0)
-    .with_effects([vfx.CrossFadeOut(1)]) #adicione 1 segundo a mais na cena, para dar tempo do fade
+    img_clip("img/background/wall.png", SCREEN_SIZE, 0, 18)
+    .with_effects([vfx.CrossFadeIn(0.5)])
 )
-interval_background = (
-    img_clip("img/questions/distinto/description1.png", SCREEN_SIZE, 18.5, 28.0)
-    .with_effects([vfx.CrossFadeIn(0.5)]) #adicione 1 segundo a mais na cena, para dar tempo do fade
+interval1_bg = (
+    img_clip("img/questions/distinto/description1.png", SCREEN_SIZE, 18, 10.76)
+    .with_effects([vfx.CrossFadeIn(0.5), vfx.CrossFadeOut(0.5)])
 )
-university_background = (
-    img_clip("img/background/room1.png", SCREEN_SIZE, 31.0, 23.0 )
-    .with_effects([vfx.CrossFadeIn(1)]) #adicione 1 segundo a mais na cena, para dar tempo do fade
-    .with_effects([vfx.CrossFadeOut(1)])
+interval2_bg = (
+    img_clip("img/questions/distinto/description2.png", (SCREEN_SIZE[0], SCREEN_SIZE[1]*0.5), 28.26, 3.24, ("center", "top"))
+    .with_effects([vfx.CrossFadeIn(0.5), vfx.CrossFadeOut(0.5)])
+)
+interval3_bg = (
+    img_clip("img/questions/distinto/description3.png", (SCREEN_SIZE[0], SCREEN_SIZE[1]*0.5), 28.26, 3.24, ("center", "bottom"))
+    .with_effects([vfx.CrossFadeIn(0.5), vfx.CrossFadeOut(0.5)])
+)
+room_bg = (
+    img_clip("img/background/room1.png", SCREEN_SIZE, 31, 17.03)
+    .with_effects([vfx.CrossFadeIn(1), vfx.CrossFadeOut(0.5)])
+)
+matrix = (
+    gif_clip("img/imgs/matrix.gif", SCREEN_SIZE, 48, 8.1)
+    .with_effects([vfx.CrossFadeOut(0.5)])
+)
+room_bg2 = (
+    img_clip("img/background/room1.png", SCREEN_SIZE, 56.1, 1.2)
+    .with_effects([vfx.CrossFadeIn(1)])
 )
 
-
-
-cat4 = (
-    cat_clip("cat4", 0, 5)
+cat1 = (
+    cat_clip("cat4", 0, 3.2)
+    .with_effects([vfx.CrossFadeIn(0.5)])
+)
+cat2 = (
+    cat_clip("cat3", 3.2, 4.35)
 )
 cat3 = (
-    cat_clip("cat3", 5, 7)
+    cat_clip("cat5", 7.55, 5.75)
+)
+cat4 = (
+    cat_clip("cat1", 13.3, 4.7)
+    .with_effects([vfx.CrossFadeOut(0.5)])
 )
 cat5 = (
-    cat_clip("cat5", 12, 6.5)
+    cat_clip("cat2", 31, 6.6)
+    .with_effects([vfx.CrossFadeIn(1)])
 )
-
 cat6 = (
-    cat_clip("cat6", 32, 11)
+    cat_clip("cat3", 37.6, 5.6)
 )
-
-
+cat7 = (
+    cat_clip("cat4", 43.2, 4.83)
+    .with_effects([vfx.CrossFadeOut(0.5)])
+)
 
 img1 = (
-    img_clip("img/questions/distinto/c.png", (1000,1000), 48.30, 0.7)
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
+    img_clip("img/questions/distinto/c.png", (1000, 1000), 48.2, 1.2)
+    .with_effects([vfx.CrossFadeIn(0.5)])
 )
 img2 = (
-    img_clip("img/questions/distinto/cpp.png", (1000,1000), 49, 1.5)
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
+    img_clip("img/questions/distinto/cpp.png", (1000, 1000), 49.4, 1.7)
 )
 img3 = (
-    img_clip("img/questions/distinto/js.png", (1000,1000), 50.5, 1.3)
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
+    img_clip("img/questions/distinto/js.png", (1000, 1000), 51.1, 1.5)
 )
 img4 = (
-    img_clip("img/questions/distinto/py.png", (1000,1000), 51.8, 1.0 )
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
+    img_clip("img/questions/distinto/py.png", (1000, 1000), 52.6, 1.5)
 )
 img5 = (
-    img_clip("img/questions/distinto/rs.png", (1000,1000), 52.8, 1.2)
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
+    img_clip("img/questions/distinto/rs.png", (1000, 1000), 54.1, 2)
+    .with_effects([vfx.CrossFadeOut(0.5)])
 )
-
-
-gif1 = (
-    gif_clip("img/imgs/matrix.gif", SCREEN_SIZE, 43, 11)
-)
-
 
 audio = audio_clip([
-    ("cena8-1", 0), # pega o cena8-1 e começa em 0 31s
-    ("cena8-2", 31.53), # pega o cena8-2 -> start definido pelo tempo no srt 27s
+    ("cena8-1", 0),
+    ("cena8-2", 31.53),
 ])
 
-subtitle = subtitle_clip("cena8") # pega o srt da cena8
+subtitle = subtitle_clip("cena8")
 
-clips = [wall_background, interval_background, university_background,
-        cat3, cat4, cat5, cat6,
-        gif1,img1, img2, img3, img4, img5,
-        subtitle
-        ] # adicione nessa lista todos os clips seguindo 
-# essa ordem: backgrounds -> cats -> imgs -> subtitle
+clips = [wall_background, interval1_bg, interval2_bg, interval3_bg, 
+         room_bg, matrix, room_bg2, cat1, cat2, cat3, cat4, cat5, 
+         cat6, cat7, img1, img2, img3, img4, img5, subtitle]
 
-video = video_clip(clips, audio.mix) # faz video com o audio e tudo que tiver no array clips
+video = video_clip(clips, audio.mix)
 
-video.write_videofile("clips/cena8.mp4", fps=4) # exporta o audio
+video.write_videofile("clips/cena8.mp4", fps=24)
