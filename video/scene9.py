fff0ed5d7cecb4464b16d24f9daedd5c8561a087
brasilicioh <brasiliciohcr@gmail.com>
2025-11-12 14:44:01 -0300

    ahnnnahnnnehhh

--- video/scene9.py
+++ video/scene9.py
@@ -4,112 +4,76 @@ from clips import (
     audio_clip, # para fazer o audio -> ja feito
     cat_clip, # retorna um ImageClip do gato
     img_clip, # retorna um ImageClip de uma imagem
-    gif_clip,
     video_clip, # junta todos ImageClip's e retorna um VideoClip
+    gif_clip,
     SCREEN_SIZE # tamanho constante de tela -> Full HD
 )
+
+university_background = (
+    img_clip("img/background/room1.png", SCREEN_SIZE, 0, 12.8)
+    .with_effects([vfx.CrossFadeIn(1)])
+    .with_effects([vfx.CrossFadeOut(1)])
+)
+wall_background = (
+    img_clip("img/background/wall.png", SCREEN_SIZE, 22.4, 12.6)
+    .with_effects([vfx.CrossFadeIn(0.5)])
+    .with_effects([vfx.CrossFadeOut(0.5)])
+)
+
+
+
+cat6 = (
+    cat_clip("cat6", 0, 4.8)
+)
+cat2 = (
+    cat_clip("cat2", 4.8, 4.2)
+)
+cat4 = (
+    cat_clip("cat4", 9, 3.8)
+)
+cat1 = (
+    cat_clip("cat1", 22.4, 5.6)
+    .with_effects([vfx.CrossFadeIn(0.5)])
+)
+cat3 = (
+    cat_clip("cat3", 28, 4, position=(200,"center"))
+    .with_effects([vfx.CrossFadeIn(0.5)])
+)
+
+
+
+
+gif1 = (
+    gif_clip("img/imgs/cyberwave.gif", SCREEN_SIZE, 12.8, 9.4)
+    .with_effects([vfx.CrossFadeIn(0.8), vfx.CrossFadeOut(0.8)])
+)
+img1 = (
+    img_clip("img/imgs/fun.jpg", (750,600), 28, 4, (1100,200))
+    .with_effects([vfx.CrossFadeIn(0.5), vfx.CrossFadeOut(0.5)])
+)
+
 
 # aqui, utilize o cat_clip para fazer gatos
 # img_clip para fazer fotos e background
 # se necessario, importe outras coisas para fazer mais clips
 # veja a cena 1 e 2 para tomar como exemplo
 
-university_background = (
-    img_clip("img/background/room1.png", SCREEN_SIZE, 0, 77.4 )
-)
-
-
-
-cat1 = (
-    cat_clip("cat1", 0, 7)
-    .with_effects([vfx.CrossFadeIn(0.5)])
-)
-cat4 = (
-    cat_clip("cat4", 7, 9)
-)
-cat5 = (
-    cat_clip("cat5", 24, 26)
-)
-cat3 = (
-    cat_clip("cat3", 50, 20,position=(200,"center"))  # gato a lateral
-)
-cat6 = (
-    cat_clip("cat6", 70, 3, position=("center", 500))
-)
-
-
-
-img1 = (
-    img_clip("img/imgs/Hacker.jpeg", SCREEN_SIZE, 16,4)
-)
-img3 = (
-    img_clip("img/imgs/windows.jpg", SCREEN_SIZE, 20,11)
-)
-img2 = (
-    img_clip("img/imgs/imgcomputer.jpg", SCREEN_SIZE, 45,50)
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
-)
-
-gif1 = (
-    gif_clip("img/imgs/matrix.gif", SCREEN_SIZE, 70, 13.4)
-    .with_effects([vfx.CrossFadeOut(0.5)])
-)
-
-leetcodelogo = (
-    img_clip("img/logo/leetcode.jpg", (500,500), 24, 6, (50,50))
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
-)
-codeforcelogo = (
-    img_clip("img/logo/codeforces.jpg", (500,500), 24, 6, (1370,600))
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
-)
-hakerranklogo = (
-    img_clip("img/logo/hackerrank.jpg", (500,500), 25, 5, (50,600))
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
-)
-beecrowdlogo = (
-    img_clip("img/logo/beecrowd.png", (500,500), 25, 5, (1370,50))
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
-)
-twosum = (
-    img_clip("img/questions/two-sum/description.png", (1000,800), 54, 15, (900,100))
-    .with_effects([vfx.CrossFadeIn(0.4), vfx.CrossFadeOut(0.4)])
-)
-
-ans1 = (
-    img_clip("img/questions/two-sum/c.png", (1000, 1000), 70, 2)
-)
-ans2 = (
-    img_clip("img/questions/two-sum/cpp.png", (1000,1000), 73, 2)
-)
-ans3 = (
-    img_clip("img/questions/two-sum/js.png", (1000,1000), 75, 2)
-)
-ans4 = (
-    img_clip("img/questions/two-sum/py.png", (1000,1000), 78, 2)
-)
-ans5 = (
-    img_clip("img/questions/two-sum/rs.png", (1000,1000), 80.4, 2)
-)
-
-
 # recomendacao: na construcao do video, coloque as variaveis em ordem cronologica
 # se lembre de colocar todas variaveis no array clips seguindo a ordem de prioridade do array
 # ao terminar a codificacao, organize as variaveis em ordem igual a ordem do array
 
-audio = audio_clip("cena9") # pega o cena9.mp3, a cena total tem 1:13 min
+audio = audio_clip("cena9") # pega o cena10.mp3
 
-subtitle = subtitle_clip("cena9") # pega o srt da cena9
+subtitle = subtitle_clip("cena9") # pega o srt da cena10
 
 clips = [
-    university_background,
-    cat1, cat4, cat5, cat6, cat3,
+    university_background, wall_background,
     gif1,
-    img1,img3, leetcodelogo, beecrowdlogo, hakerranklogo, codeforcelogo, 
-    twosum, ans1,ans2,ans3,ans4,ans5,     
-    subtitle     
-        ] # adicione nessa lista todos os clips seguindo 
-# essa ordem: backgrounds -> cats -> imgs -> subtitle
+    cat6, cat4, cat2,cat3, cat1,
+    img1,
+    subtitle
+    ] # adicione nessa lista todos os clips seguindo 
+                   # essa ordem: backgrounds -> cats -> imgs -> subtitle
 
 video = video_clip(clips, audio.mix) # faz video com o audio e tudo que tiver no array clips
 
