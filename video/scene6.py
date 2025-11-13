5bf69a03fc779c8cc08fa02f64bf4654bbd3e677
brasilicioh <brasiliciohcr@gmail.com>
2025-11-13 10:45:34 -0300

    update final scenes

--- video/scene6.py
+++ video/scene6.py
@@ -1,11 +1,11 @@
-from moviepy import vfx # vfx para efeitos crossfadein/out; se necessario, separado por virgula, importe mais coisas
+from moviepy import vfx 
 from clips import (
-    subtitle_clip, # para fazer legendas -> ja feito
-    audio_clip, # para fazer o audio -> ja feito
-    cat_clip, # retorna um ImageClip do gato
-    img_clip, # retorna um ImageClip de uma imagem
-    video_clip, # junta todos ImageClip's e retorna um VideoClip
-    SCREEN_SIZE # tamanho constante de tela -> Full HD
+    subtitle_clip,
+    audio_clip,
+    cat_clip,
+    img_clip,
+    video_clip,
+    SCREEN_SIZE
 )
 
 # 
@@ -13,18 +13,14 @@ from clips import (
 def ease_inout(t: float) -> float:
     return 2 * t * t if t < 0.5 else 1 - (-2 * t + 2)**2 / 2
 
-# aqui, utilize o cat_clip para fazer gatos
-# img_clip para fazer fotos e background
-# se necessario, importe outras coisas para fazer mais clips
-# veja a cena 1 e 2 para tomar como exemplo
-
-# recomendacao: na construcao do video, coloque as variaveis em ordem cronologica
-# se lembre de colocar todas variaveis no array clips seguindo a ordem de prioridade do array
-# ao terminar a codificacao, organize as variaveis em ordem igual a ordem do array
 
 room_background = (
     img_clip("img/background/room1.png", SCREEN_SIZE, 0, 56.45)
-    .with_effects([vfx.CrossFadeIn(1)])
+    .with_effects([vfx.CrossFadeOut(0.5)])
+)
+school_bg = (
+    img_clip("img/background/school.png", SCREEN_SIZE, 56.4, 0.9)
+    .with_effects([vfx.CrossFadeIn(0.5)])
 )
 
 cat1 = (
@@ -42,7 +38,6 @@ cat3 = (
 cat4 = (
     cat_clip("cat3", 11.75, 1.7) # 25
 )
-
 cat5 = (
     cat_clip("cat5", 13.45, 18.05) # until 1.3700
     .with_position(lambda t: (
@@ -50,18 +45,18 @@ cat5 = (
         max(SCREEN_SIZE[0]/2 - 300 - 10 - 640*ease_inout(min(t/2, 1)), 10),
         "center"))
 )
-
 cat6 = (
     cat_clip("cat2", 31.5, 24.95)
     .with_position(lambda t: (
         # this makes sense pls, don't mess with it
         (SCREEN_SIZE[0]/2 - 300 - 10) - (max(SCREEN_SIZE[0]/2 - 300 - 10 - 640*ease_inout(min(t/2, 1)), 430)),
         "center"))
+    .with_effects([vfx.CrossFadeOut(0.5)])
 )
 
 mundo = (
-    img_clip("img/imgs/mundo.jpg", (SCREEN_SIZE[0]*0.3, SCREEN_SIZE[1]*0.3),
-             5, 3, (SCREEN_SIZE[0]*5/8, "center"))
+    img_clip("img/imgs/mundo.jpg", (SCREEN_SIZE[0]*0.6, SCREEN_SIZE[1]*0.6),
+             5, 3, ("center", "center"))
     .with_effects([vfx.CrossFadeIn(0.5), vfx.CrossFadeOut(0.25)])
 )
 
@@ -81,11 +76,11 @@ icpc_logo = (
 )
 obi_logo = (
     img_clip("img/logo/obi.png", (SCREEN_SIZE[0]*0.2, SCREEN_SIZE[1]*0.3),
-             11, 1.7, (200, 770))
+             11, 1.7, (200, 580))
 )
 ioi_logo = (
     img_clip("img/logo/ioi.jpg", (SCREEN_SIZE[0]*0.3, SCREEN_SIZE[1]*0.3),
-             11.8, 0.9, (1144, 770))
+             11.8, 0.9, (1144, 580))
 )
 
 # equipe de três pessoas
@@ -127,30 +122,29 @@ ballon2 = (
 
 sbc_pessoas = (
     img_clip("img/imgs/sbc_pessoas.jpg", ((SCREEN_SIZE[0]*0.5, SCREEN_SIZE[1] * 0.5)),
-             40, 5, (SCREEN_SIZE[0]*(1/2 - 1/32), "center"))
+             39.2, 5.8, (SCREEN_SIZE[0]*(1/2 - 1/32), "center"))
     .with_effects([vfx.CrossFadeIn(0.5), vfx.CrossFadeOut(0.5)])
 )
 
 icpc_competition = (
     img_clip("img/imgs/icpc_teams.jpg", (SCREEN_SIZE[0]*0.5, SCREEN_SIZE[1]*0.5),
              46, 10.45, (SCREEN_SIZE[0]*(1/2 - 1/32), "center"))
-    .with_effects([vfx.CrossFadeIn(0.5), vfx.CrossFadeOut(0.25)])
+    .with_effects([vfx.CrossFadeIn(0.5), vfx.CrossFadeOut(0.5)])
 )
 
-audio = audio_clip("cena6") # pega o cena6.mp3
+audio = audio_clip("cena6")
 
-subtitle = subtitle_clip("cena6") # pega o srt da cena6
+subtitle = subtitle_clip("cena6")
 
-clips = [room_background,
+clips = [room_background, school_bg,
          cat1, cat2, cat3, cat4, cat5, cat6,
          mundo, sbc_competition,
          maratona_logo, icpc_logo, obi_logo, ioi_logo,
          equipe_pessoa1, equipe_pessoa2, equipe_pessoa3,
          sbc_pessoas, icpc_competition,
          ballon1, ballon2,
-         subtitle] # adicione nessa lista todos os clips seguindo 
-                   # essa ordem: backgrounds -> cats -> imgs -> subtitle
+         subtitle]
 
-video = video_clip(clips, audio.mix) # faz video com o audio e tudo que tiver no array clips
+video = video_clip(clips, audio.mix)
 
-video.write_videofile("clips/cena6.mp4", fps=24, threads=32) # exporta o audio
+video.write_videofile("clips/cena6.mp4", fps=24)
